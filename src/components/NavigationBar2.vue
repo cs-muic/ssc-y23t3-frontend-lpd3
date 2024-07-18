0cbb9ed0080674f7f212fba47a17a5dba25dc812
Mynameisdaves <145308523+Mynameisdaves@users.noreply.github.com>
2024-07-18 23:03:28 +0700

    Added more navigation bars for navigation purposes

---
+++ src/components/NavigationBar2.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="topnav">
+    <router-link to="/playerdata" class="btn btn-primary">Back to Player data</router-link>
+    <div class="topnav-right">
+      <router-link to="/login" class="btn btn-outline-light">Sign out</router-link>
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+// Import statements or script setup if needed
+</script>
+
+<style scoped>
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav-right{
+  float: right;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+
---
+++ src/components/NavigationBar3.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="topnav">
+    <router-link to="/tournamentlist" class="btn btn-primary">Back to Tournament List</router-link>
+    <div class="topnav-right">
+      <router-link to="/login" class="btn btn-outline-light">Sign out</router-link>
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav-right{
+  float: right;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
